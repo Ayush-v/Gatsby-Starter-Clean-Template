bc12a3ef6a664dbf8b2c5e98bcc3f0b21b3ce086
Ayush <62958067+Ayush-v@users.noreply.github.com>
2021-06-30 17:00:45 +0530

    Update HeroSection.js

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,10 +1,15 @@
 import React from "react"
+import styled from "styled-components"
 
 export default function HeroSection() {
   return (
-    <div>
+    <Wrapper>
       <h1>Ayush</h1>
       <p>this is your clean template</p>
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+color: white;
+`
