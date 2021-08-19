f3cc8a4bb1e25f11fa4305aaf800b9361f15722b
Ayush <62958067+Ayush-v@users.noreply.github.com>
2021-08-20 04:44:19 +0530

    Update GlobalStyles.js

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -4,9 +4,11 @@ import { themes } from "./ColorStyles"
 export const GlobalStyle = createGlobalStyle`
 body {
   background: ${themes.light.backgroundColor};
+  color: ${themes.light.text1};
 
   @media (prefers-color-scheme: dark) {
     background: ${themes.dark.backgroundColor};
+    color: ${themes.dark.text1};
   }
 }
 `
